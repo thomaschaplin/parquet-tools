0e1a1e89cd72e873ec1cb08a50fbe06ab79d0a0f
Thomas Chaplin <35810778+thomaschaplin@users.noreply.github.com>
2019-07-17 17:11:24 +0100

    Update parquet-to-folder.py

--- parquet-to-folder.py
+++ parquet-to-folder.py
@@ -2,6 +2,12 @@ import pandas
 import pyarrow.parquet as pq
 
 if __name__ == "__main__":
+    # Write parquet file to a dataset
     df = pq.read_pandas('data.snappy.parquet')
     data = pq.read_table('data.snappy.parquet')
     pq.write_to_dataset(data, root_path='dataset', partition_cols=['FIRSTNAME'])
+
+    # Read parquet files from a dataset
+    dataset = pq.ParquetDataset('dataset/')
+    data_from_dataset = dataset.read().to_pandas()
+    print(data_from_dataset)
