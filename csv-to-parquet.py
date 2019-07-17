8659bc739e3af1d5e6f5c922b0356f0cc2330f18
Thomas Chaplin <35810778+thomaschaplin@users.noreply.github.com>
2019-07-17 15:13:59 +0100

    Add files via upload

---
+++ csv-to-parquet.py
@@ -0,0 +1,8 @@
+import pandas
+import pyarrow.parquet as pq
+
+if __name__ == "__main__":
+    df = pandas.read_csv('data.csv')
+    df.to_parquet('data.snappy.parquet', engine='auto', compression='snappy')
+    data = pq.read_pandas('data.snappy.parquet').to_pandas()
+    print(data)
