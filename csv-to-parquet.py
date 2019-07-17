642562a09f6c13b37496b8483ecb8f1421d8e520
Thomas Chaplin <35810778+thomaschaplin@users.noreply.github.com>
2019-07-17 17:20:02 +0100

    Update csv-to-parquet.py

--- csv-to-parquet.py
+++ csv-to-parquet.py
@@ -4,6 +4,6 @@ import pyarrow.parquet as pq
 if __name__ == "__main__":
     df = pandas.read_csv('data.csv')
     df.to_parquet('data.snappy.parquet', engine='auto',
-                  compression='SNAPPY')  # 'NONE', 'SNAPPY', 'GZIP', 'LZO', 'BROTLI', 'LZ4', 'ZSTD'
+                  compression='snappy')  # 'snappy', 'gzip', 'brotli', 'None'
     data = pq.read_pandas('data.snappy.parquet').to_pandas()
     print(data)
